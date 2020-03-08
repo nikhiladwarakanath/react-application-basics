d9743cbf42bd6a0a8cb9b8344d72bd1f22a20058
nikhiladwarakanath <nikhila.dwarakanath@nyu.edu>
2020-03-08 18:53:53 -0400

    props segment complete

--- components/src/index.js
+++ components/src/index.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                    <h4>Warning</h4>
+                    Are you sure you want to continue?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    date="Today at 3:15 am"
+                    text="hello there"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    date="Today at 6:22 pm"
+                    text="well done"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    date="Today at 12:00 pm"
+                    text="how is it going?"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
